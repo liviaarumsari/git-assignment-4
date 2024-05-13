15093ab385302134dbb8034a6ee599a2f173e91a
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-13 22:15:24 +0700

    refactor: index html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "*.html",
     "./pages/*.html",
     "./components/*.html",
     "./src/**/*.{js,ts,jsx,tsx}",
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   build: {
       rollupOptions: {
           input: {
-              app: './pages/index.html',
+              main: './index.html',
+              'about-us': './pages/about-us.html',
+              'contact-us': './pages/contact-us.html',
+              blog: './pages/blog.html',
+              portfolio: './pages/portfolio.html',
+              todo: './pages/todo.html',
+              nav: './components/nav.html',
+              footer: './components/footer.html',
           },
       },
   },
 server: {
     open: '/pages/index.html',
 },
+    plugins: [],
+    resolve: {
+        /*something*/
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss],
+        },
+    },
 })
